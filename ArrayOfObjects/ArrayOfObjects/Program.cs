a395bfa1554349e59b16952e189701e9b563d6e2
Darkwolf <darkwolf999@mail.ru>
2019-10-06 23:16:39 +0300

    Adding ArrayOfObjects project

---
+++ ArrayOfObjects/ArrayOfObjects/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=> Array of Objects.");
+            // Массив объектов может содержать все что угодно,
+            object[] myObjects = new object[4];
+            myObjects[0] = 10;
+            myObjects[1] = false;
+            myObjects[2] = new DateTime(1969, 3, 24);
+            myObjects[3] = "Form & Void";
+            foreach (object obj in myObjects)
+            {
+                // Вывести тип и значение каждого элемента в массиве.
+                Console.WriteLine("Туре: {0}, Value: {1}", obj.GetType(), obj);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
