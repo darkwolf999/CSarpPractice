12943325a65b6f7e031d43562bfbfb1ff6b68658
Darkwolf <darkwolf999@mail.ru>
2019-10-06 22:49:46 +0300

    Adding RefTypeValTypeParams project

---
+++ RefTypeValTypeParams/RefTypeValTypeParams/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefTypeValTypeParams
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person() { }
+
+        public void Display()
+        {
+            Console.WriteLine($"name = {name}, age = {age}");
+        }
+    }
+}
---
+++ RefTypeValTypeParams/RefTypeValTypeParams/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RefTypeValTypeParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Start name", 12);
+
+            p.Display();
+
+            SendAPersonByValue(p);
+
+            p.Display();
+
+            SendAPersonByRef(ref p);
+
+            p.Display();
+
+            Console.ReadKey();
+        }
+
+        static void SendAPersonByValue(Person per)
+        {
+            per.age = 99999999;
+
+            per = new Person("new name", 11111);
+        }
+
+        static void SendAPersonByRef(ref Person per)
+        {
+            per.age = 88888888;
+
+            per = new Person("new name", 11111);
+        }
+    }
+}
