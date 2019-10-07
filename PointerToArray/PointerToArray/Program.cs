bb20cddfc06c55a7d030b7aad2d3e1e5d359cce5
Darkwolf <darkwolf999@mail.ru>
2019-10-07 11:31:20 +0300

    Adding PointerToArray project

---
+++ PointerToArray/PointerToArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PointerToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "one", "two", "three" };
+            ref string[] pointer = ref array;
+
+            foreach (string element in array)
+                Console.WriteLine(element);
+
+            pointer[1] = "null";
+
+            Console.WriteLine();
+
+            foreach (string element in array)
+                Console.WriteLine(element);
+
+            for (int i = 0; i < pointer.Length; i++)
+                pointer[i] = "null";
+
+            Console.WriteLine();
+
+            foreach (string element in array)
+                Console.WriteLine(element);
+
+
+
+
+
+            Console.WriteLine();
+
+            int y = 10;
+            Console.WriteLine(y);
+            ref int x = ref y;
+            x = 11;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.ReadKey();
+        }
+    }
+}
