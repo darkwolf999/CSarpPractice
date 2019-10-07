dd739112b7fd261335d4077af7bda324d5a1f723
Darkwolf <darkwolf999@mail.ru>
2019-10-07 19:30:08 +0300

    Adding PointerToArrayReturnParams project

---
+++ PointerToArrayReturnParams/PointerToArrayReturnParams/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PointerToArrayReturnParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "ass", "we", "can" };
+
+            foreach(string element in arr)
+                Console.WriteLine(element);
+
+            ref string pointer = ref SampleRefReturn(arr, 1);
+            pointer = "he";
+
+            Console.WriteLine();
+
+            foreach (string element in arr)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+
+        static ref string SampleRefReturn(string[] array, int position)
+        {
+            return ref array[position];
+        }
+
+
+
+
+    }
+}
