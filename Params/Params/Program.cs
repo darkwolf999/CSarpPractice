f53f7de05be9a248ab81eac8636dd195e408232d
Darkwolf <darkwolf999@mail.ru>
2019-10-07 20:33:05 +0300

    Adding Params project

---
+++ Params/Params/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateAvg());
+
+            Console.ReadKey();
+        }
+
+        static int CalculateAvg(params int[] values)
+        {
+            int sum = 0;
+            if (values.Length == 0)
+                return sum;
+            foreach (int value in values)
+                sum += value;
+            return sum / values.Length;
+        }
+    }
+}
