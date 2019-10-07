4faabef52e5db859e98fe3d416576840cb696f9f
Darkwolf <darkwolf999@mail.ru>
2019-10-07 21:04:15 +0300

    Adding OptionalParams project

---
+++ OptionalParams/OptionalParams/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OptionalParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EnterLogData("503");
+
+            Console.WriteLine();
+
+            EnterLogData("503", "User");
+
+            Console.Read();
+        }
+
+        static void EnterLogData(string message, string owner = "Programmer")
+        {
+            Console.Beep();
+            Console.WriteLine("Error: {0}", message);
+            Console.WriteLine("Owner of Error: {0}", owner);
+        }
+
+    }
+}
