ad5f6725ec9c05b960605ef6ab339c1324100168
Darkwolf <darkwolf999@mail.ru>
2019-10-20 19:50:05 +0300

    Adding project

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "priveti";
+            string str2 = "priveti0";
+
+            Console.WriteLine(str);
+
+            str = str.ToUpper();
+
+            Console.WriteLine(str);
+
+            str = str.ToLower();
+
+            Console.WriteLine(str);
+
+            Console.WriteLine(str.CompareTo(str2));
+
+
+            char[] arrChar = { 'c', 'a', 'r' };
+            string strFromCharArr = new string(arrChar);
+            Console.WriteLine(strFromCharArr);
+
+
+            Console.Read();
+        }
+    }
+}
