d451cf4a596a8e0320908b2c6000aca9e79ab961
Darkwolf <darkwolf999@mail.ru>
2019-12-09 05:15:14 +0300

    Add project

---
+++ Deafult/Deafult/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Deafult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = default;
+            double d = default;
+            char c = default;
+            string s = default;
+            bool b = default;
+
+            Console.WriteLine($"int: {i}\ndouble: {d}\nchar: {c}\nstring: {s}\nbool: {b}");
+
+            Console.Read();
+        }
+    }
+}
