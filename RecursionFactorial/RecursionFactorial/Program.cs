c79996731f1d3099081dae87c7219fd488cd865b
Darkwolf <darkwolf999@mail.ru>
2020-01-07 06:27:29 +0300

    Adding project

---
+++ RecursionFactorial/RecursionFactorial/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace RecursionFactorial
+{
+    class Program
+    {
+        class Factorial
+        {
+            // Это рекурсивный метод.
+            public int FactR(int n)
+            {
+                int result;
+                if (n == 1) return 1;
+                result = FactR(n - 1) * n;
+                return result;
+            }
+
+
+            // Это итерационный метод.
+            public int FactI(int n)
+            {
+                int t, result;
+                result = 1;
+                for (t = 1; t <= n; t++) result *= t;
+                return result;
+            }
+        }
+
+        class RevStr
+        {
+            // Вывести символьную строку в обратном порядке.
+            public void DisplayRev(string str)
+            {
+                if (str.Length > 0)
+                    DisplayRev(str.Substring(1, str.Length - 1));
+                else
+                    return;
+                Console.Write(str[0]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Factorial f = new Factorial();
+            Console.WriteLine("Факториалы, рассчитанные рекурсивным методом.");
+            Console.WriteLine("Факториал числа 3 равен " + f.FactR(3));
+            Console.WriteLine("Факториал числа 4 равен " + f.FactR(4));
+            Console.WriteLine("Факториал числа 5 равен " + f.FactR(5));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Факториалы, рассчитанные итерационным методом.");
+            Console.WriteLine("Факториал числа 3 равен " + f.FactR(3));
+            Console.WriteLine("Факториал числа 4 равен " + f.FactR(4));
+            Console.WriteLine("Факториал числа 5 равен " + f.FactR(5));
+
+            Console.WriteLine();
+
+            string s = "Это тест";
+            RevStr rsOb = new RevStr();
+            Console.WriteLine("Исходная строка: " + s);
+            Console.Write("Перевернутая строка: ");
+            rsOb.DisplayRev(s);
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
