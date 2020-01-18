2279fe26999da805a57fbba39d504846c309faac
Darkwolf <darkwolf999@mail.ru>
2020-01-18 19:42:39 +0300

    Adding project

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        public static int FindFactorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return FindFactorial(n - 1) * n;
+        }
+
+        static void Main(string[] args)
+        {
+            int fact = FindFactorial(10);
+        }
+    }
+}
