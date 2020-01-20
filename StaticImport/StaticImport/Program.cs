23959b52ce1e7e8d6bf9a31f1f7b4b0c2ba0347b
Darkwolf <darkwolf999@mail.ru>
2020-01-20 23:23:23 +0300

    Adding project

---
+++ StaticImport/StaticImport/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using static System.Console; // Добавь этот импорт
+
+namespace StaticImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");// И теперь можно писать без приставки Console.
+        }
+    }
+}
