312fafd7cbec885c6128783bdefe2e0646231081
Darkwolf <darkwolf999@mail.ru>
2020-01-20 23:16:51 +0300

    Adding project

---
+++ ShortMethods/ShortMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShortMethods
+{
+    class Program
+    {
+        static void someFunc() => Console.WriteLine("This is short method");
+
+        static void Main(string[] args)
+        {
+            someFunc();
+        }
+    }
+}
