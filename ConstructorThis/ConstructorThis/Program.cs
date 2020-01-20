834f4913ecd22c1a430aec9da606adbfa8950429
Darkwolf <darkwolf999@mail.ru>
2020-01-20 22:53:50 +0300

    Adding motorcycle class

--- ConstructorThis/ConstructorThis/Program.cs
+++ ConstructorThis/ConstructorThis/Program.cs
@@ -23,6 +23,57 @@ namespace ConstructorThis
         }
     }
 
+
+
+    class Motorcycle
+    {
+        public int driverlntensity;
+        public string driverName;
+        // Связывание конструкторов в цепочку,
+        public Motorcycle()
+        {
+            Console.WriteLine("In default constructor");
+            // Внутри стандартного конструктора
+        }
+        public Motorcycle(int intensity) : this(intensity, "")
+        {
+            Console.WriteLine("In constructor taking an int");
+            // Внутри конструктора, принимающего int
+        }
+        public Motorcycle(string name)
+        : this(0, name)
+        {
+            Console.WriteLine("In constructor taking a string");
+            // Внутри конструктора, принимающего string
+        }
+        // Это 'главный' конструктор, выполняющий всю реальную работу,
+        public Motorcycle(int intensity, string name)
+        {
+            Console.WriteLine("In master constructor ");
+            // Внутри главного конструктора
+            if (intensity > 10)
+            {
+                intensity = 10;
+            }
+            driverlntensity = intensity;
+            driverName = name;
+        }
+
+        public void SetDriverName(string name)
+        {
+            driverName = name;
+        }
+
+        public void PopAWheely()
+        {
+            for (int i = 1; i <= driverlntensity; i++)
+            {
+                Console.WriteLine("Yeeeeeee Haaaaaeewww!");
+            }
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -34,6 +85,18 @@ namespace ConstructorThis
             Console.WriteLine("t1.x, t1.y: " + t1.x + ", " + t1.y);
             Console.WriteLine("t2.x, t2.y: " + t2.x + ", " + t2.y);
             Console.WriteLine("t3.x, t3.y: " + t3.x + ", " + t3.y);
+
+
+
+
+            Console.WriteLine("*****Fun with class Types *****\n");
+            // Создать объект Motorcycle.
+            Motorcycle c = new Motorcycle(5);
+            c.SetDriverName("Tiny");
+            c.PopAWheely();
+            Console.WriteLine("Rider name is {0}", c.driverName); //вывод имени гонщика
+            Console.ReadLine();
+
         }
     }
 }
