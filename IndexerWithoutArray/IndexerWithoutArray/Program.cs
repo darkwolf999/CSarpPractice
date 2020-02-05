f59fc8c7cae79d1f872b1de584efb82eef3b7548
Darkwolf <darkwolf999@mail.ru>
2020-02-05 03:00:43 +0300

    Adding project

---
+++ IndexerWithoutArray/IndexerWithoutArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IndexerWithoutArray
+{
+    class PowOfTwo
+    {
+        public int this[int index]
+        {
+            get
+            {
+                return PowFunc(index);
+            }
+        }
+
+        private int PowFunc(int index)
+        {
+            int result = 1;
+
+            for (int i = 0; i < index; i++)
+            {
+                result *= 2;
+            }
+
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowOfTwo p = new PowOfTwo();
+
+            Console.WriteLine(p[10]);
+        }
+    }
+}
